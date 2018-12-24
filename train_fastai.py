de74056c7ce4e542ebae40819ebea90efc323aa1
Jieren Chen <jieren.chen@gmail.com>
2018-12-24 16:39:32 +0700

    fixes

--- train_fastai.py
+++ train_fastai.py
@@ -2,7 +2,7 @@ from fastai.basic_train import Learner
 from torch.utils.data import DataLoader
 from fastai.basic_data import DataBunch
 
-def train_fastai(training_data, validation_data, model, epochs, learning_rate)
+def train_fastai(training_data, validation_data, model, epochs, learning_rate):
   db = DataBunch(
     DataLoader(training_data, batch_size=8), 
     DataLoader(validation_data, batch_size=8)
